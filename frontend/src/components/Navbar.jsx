604b0c864cba6959b0c56448d6dcc378d23b7091
Asamoah Yeboah Isaac <asamoah1357@gmail.com>
2023-07-04 16:08:26 -0700

    navbar design for sm, md  and lg screen

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,11 +7,11 @@ import ToggleSwitch from './ToggleSwitch'
 
 export const Navbar = () => {
   return (
-        <nav className="nav flex justify-between items-center h-[160px] px-8 ">
-            <div>
+        <nav className="nav relative px-12 flex justify-between items-center h-[160px] md:px-20 ">
+            <div className='lg:px-24'>
               <a href="/" title='HomePage'> <img src={logo} alt="devjobslogo" /></a>
-            </div>
-            <div className='flex items-center gap-2 '>
+            </div >
+            <div className='flex items-center sm:gap-2 sm:px-4 lg:px-[70px] lg:gap-2 '>
               {/* sunlogo  */}
               <div>
                 <img src={sun} alt="sunlogo" />
