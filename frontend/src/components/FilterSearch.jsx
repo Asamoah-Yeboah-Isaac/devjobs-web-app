b87d61b24dc64e4463a23298c1886899888d2b82
Asamoah Yeboah Isaac <asamoah1357@gmail.com>
2023-07-05 04:05:02 -0700

    filterbar design

--- frontend/src/components/FilterSearch.jsx
+++ frontend/src/components/FilterSearch.jsx
@@ -9,19 +9,24 @@ const FilterSearch = () => {
       <div className="relative mx-12 lg:mx-0 ">
         <input
           type="text"
-          className="lg:relative bg-white pl-10 pr-2 py-2 text-sm border border-gray-300 focus:outline-none focus:ring-2 h-20 w-96 lg:w-[500px] lg:-pl-8  "
+          className="lg:relative bg-white pl-10 pr-2 py-2 text-sm border-r-2  focus:outline-none focus:ring-2 h-20 w-96 lg:w-[500px] lg:-pl-8  "
           placeholder="Filter by titles, companies, expertise..."
         />
-        <div className=" bg-white absolute right-10 sm:p-4 top-1/2 transform -translate-y-1/2 ">
+        <div className=" bg-white absolute  right-10 sm:p-4 top-1/2 transform -translate-y-1/2 flex items-center ">
           <img
             src={filtericon}
             alt="filterIcon"
-            className="h-4 w-4 inline-block mr-2 bg-white md:hidden "
+            className="   h-4 w-4 md:inline-block mr-2 bg-white  "
           />
-          <img
+          <img //lg search image
             src={Searchicon}
             alt="searchIcon"
-            className="h-4 w-4 inline-block bg-white lg:absolute "
+            className="h-4 w-4 hidden md:inline-block bg-white lg:absolute "
+          />
+          <img //smaller screen image
+            src={Searchicon}
+            alt="searchIcon"
+            className=" md:hidden"
           />
         </div>
       </div>
@@ -29,13 +34,13 @@ const FilterSearch = () => {
       <div className="relative hidden md:flex">
         <input
           type="text"
-          className="pl-10 pr-4 py-2 lg:w-[320px] text-sm border border-gray-300 focus:outline-none focus:ring-2 "
+          className="pl-10 pr-4 py-2 lg:w-[320px] text-sm border-r-2  focus:outline-none focus:ring-2 "
           placeholder="Filter by location..."
         />
         <img
           src={locationLogo}
           alt="locationIcon"
-          className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4" // ade9b23b2cabc1b3cb79c5d6d63eacfa1147338e
+          className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4" //
         />
       </div>
 
@@ -43,8 +48,8 @@ const FilterSearch = () => {
       <div className="md:flex items-center  mx-3 lg:mx-0 lg:align-middle hidden lg:bg-white lg:relative lg:px-7  ">
         <div className="flex items-center lg:w-80 lg:px-13 lg:-pl-5">
           <input type="checkbox" className="w-6 h-6  " />
-          <div className="mr-3 ml-2 flex justify-between text-slate-500 font-bold lg:ml- ">
-            Full Time Only
+          <div className="mr-3 flex justify-between ml-2 text-slate-500 font-bold  ">
+            <span className="hidden lg:inline ">Full Time Only</span>
           </div>
           <button
             type="button"
@@ -59,3 +64,5 @@ const FilterSearch = () => {
 };
 
 export default FilterSearch;
+
+// flex items-center lg:w-80 lg:px-13 lg:-pl-5
