90a37c1496a7c602ea0cb1e71c9213708f33d7f2
Asamoah Yeboah Isaac <asamoah1357@gmail.com>
2023-07-05 03:01:02 -0700

    fix background color and typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,20 @@
-import './index.css'
- import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import { Home } from './pages/Home'
-import { JobDesc } from './pages/JobDesc'
-import { NotFoundPage } from './pages/NotFoundPage'
-import { Navbar } from './components/Navbar'
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { JobDesc } from "./pages/JobDesc";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import { Navbar } from "./components/Navbar";
 function App() {
-
   return (
     <BrowserRouter>
-    {/* <Navbar /> */}
-    <Routes>
-    <Route element={<Home/>} path='/' />
-    <Route element={<JobDesc/>} path='/jobs' />
-    <Route element={<NotFoundPage/>} path='*' />
-    </Routes>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<JobDesc />} path="/job" />
+        <Route element={<NotFoundPage />} path="*" />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
