d6c5fd80896fe0e413333073d7eec64ed56f4398
Asamoah Yeboah Isaac <asamoah1357@gmail.com>
2023-07-04 17:26:34 -0700

    Revert "update package files"
    This reverts commit ade9b23b2cabc1b3cb79c5d6d63eacfa1147338e.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,11 @@
 import './index.css'
- import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import { Home } from './pages/Home'
-import { JobDesc } from './pages/JobDesc'
-import { NotFoundPage } from './pages/NotFoundPage'
-import { Navbar } from './components/Navbar'
+
 function App() {
 
   return (
-    <BrowserRouter>
-    {/* <Navbar /> */}
-    <Routes>
-    <Route element={<Home/>} path='/' />
-    <Route element={<JobDesc/>} path='/jobs' />
-    <Route element={<NotFoundPage/>} path='*' />
-    </Routes>
-    </BrowserRouter>
+    <>
+      
+    </>
   )
 }
 
