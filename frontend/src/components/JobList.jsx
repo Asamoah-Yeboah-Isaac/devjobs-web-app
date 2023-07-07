cf770df46405a963d39a3288cef1cf744096eaea
Asamoah Yeboah Isaac <asamoah1357@gmail.com>
2023-07-07 03:13:19 -0700

    position and size adjustment to jobcards

--- frontend/src/components/JobList.jsx
+++ frontend/src/components/JobList.jsx
@@ -3,10 +3,13 @@ import { logos } from "../../logos";
 logos;
 export const JobList = ({ jobData }) => {
   return (
-    <section className="grid pt-12 lg:grid-cols-3 md:grid-cols-2 gap-14 rounded-md items-center justify-center md:mx-10   cursor-pointer">
+    <section className="grid pt-12 lg:grid-cols-3 md:grid-cols-2 gap-14 lg:gap-x-[0px] lg:pl-24 rounded-md items-center justify-center md:mx-10  cursor-pointer">
       {jobData.map((job) => {
         return (
-          <div className=" bg-white w-96 pl p-12 relative" key={job.id}>
+          <div
+            className=" bg-white w-96 pl p-12 md:w-[330px] relative"
+            key={job.id}
+          >
             <span className="text-[16px] font-normal bg-white text-slate-500">
               {job.contract} <span className="bg-white ml-1 mr-1">.</span>{" "}
               {job.postedAt}
@@ -16,7 +19,7 @@ export const JobList = ({ jobData }) => {
               {" "}
               {job.company}
             </h2>
-            <p className="w-[286px] text-indigo-500 text-[14px] font-bold bg-white -mb-3">
+            <p className="w-[286px] md:w-[270px] text-indigo-500 text-[14px] font-bold bg-white -mb-3">
               {job.location}
             </p>{" "}
             <div
