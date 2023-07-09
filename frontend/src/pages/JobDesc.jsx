91f2213c0495478adffa71c03381f036e88876f6
Asamoah Yeboah Isaac <asamoah1357@gmail.com>
2023-07-09 06:55:34 -0700

    Job description and footer design for sm

--- frontend/src/pages/JobDesc.jsx
+++ frontend/src/pages/JobDesc.jsx
@@ -25,19 +25,18 @@ export const JobDesc = ({ isDarkMode }) => {
 
   // custom background colors are used for styling
   return (
-    <div>
+    <div className="colorLightGray">
       <Navbar />
-      <section 
-        className={`colorLightGray  relative z-50 flex flex-col items-center  justify-center gap-5  md:gap-20`}
-        
-      > 
-      {/* comapny site,company name ,logo */}
+      <section
+        className={`colorLightGray  relative z-50 flex flex-col items-center  justify-center gap-5  md:gap-10`}
+      >
+        {/* comapny site,company name ,logo */}
         <div
           className={`flex flex-col items-center justify-between -mt-5  rounded-lg md:h-auto md:flex-row md:pt-4 h-[250px]  ${
             isDarkMode ? " border-b-red-400" : " bg-white"
           }`}
         >
-          <div className="flex flex-col items-center gap-3 -mt-4 md:flex-row md:h-24 md:gap-6 w-[380px]  ">
+          <div className="flex flex-col items-center gap-3 -mt-4 md:flex-row md:h-32 md:gap md:gap-6 w-[380px] md:w-[640px]">
             <div
               className="flex items-center self-start justify-center p-6 mx-4 rounded-lg md:rounded-none md:px-8 md:mx-0  "
               style={{ backgroundColor: job?.backgroundColor }}
@@ -48,6 +47,7 @@ export const JobDesc = ({ isDarkMode }) => {
                 alt={` The logo of ${job?.company}`}
               />
             </div>
+
             {/* comapany site styles  */}
             <div className="flex flex-col items-center justify-center gap-1 ">
               <p
@@ -57,46 +57,50 @@ export const JobDesc = ({ isDarkMode }) => {
               >
                 {job?.company}
               </p>
-              <p className="font-medium " style={{color:'#6E8089' }}>
+              <p className="font-medium " style={{ color: "#6E8089" }}>
                 {`${job?.company}.com`.toLowerCase()}
               </p>
             </div>
           </div>
-                     {/* company site  */}
+
+          {/* company site  */}
           <p
             className={`p-3 mb-6 ml-3 text-sm font-semibold tracking-wider  md:mr-6 rounded-md${
               isDarkMode
                 ? " bg-gray-800 hover:bg-gray-700 hover:text-white"
                 : " colorLightGray  hover:bg-gray-200 "
             }`}
-            style={{color:'#939BF4' }}
+            style={{ color: "#939BF4" }}
           >
             <Link to={job?.website}> Company Site</Link>
           </p>
         </div>
-         {/* details,apply, description and requirement of job  */}
+
+        {/* postedAt,contract,apply, description and requirement of job  */}
         <div
-          className={`p-8 w-[380px] ${
+          className={`p-8 w-[380px] md:w-[790px]  ${
             isDarkMode ? " bg-red-500" : " bg-white"
           }`}
         >
-          <div className="flex flex-col justify-between gap-10 md:gap-0 md:items-center md:flex-row ">
+          <div className="flex flex-col justify-between gap-10 md:gap-0 md:items-center md:flex-row  ">
             <div className="flex flex-col gap-2">
               <div className="flex gap-2 font-normal leading-4 ">
                 <p>{job?.postedAt}</p> <p>.</p>
                 <p>{job?.contract}</p>
               </div>
+
               <p
                 className={`text-lg font-bold leading-6 md:text-2xl   ${
                   isDarkMode ? " text-white " : " text-blue-900 "
                 }`}
-                style={{color:'#121721'}}
+                style={{ color: "#121721" }}
               >
                 {job?.position}
               </p>
               <p className="text-sm font-semibold leading-5 text-violet-700">
                 {job?.location}
               </p>
+
               {/* first apply button  */}
             </div>
             <Link to={job?.apply}>
@@ -105,7 +109,11 @@ export const JobDesc = ({ isDarkMode }) => {
               </button>
             </Link>
           </div>
-          <p className="py-8 leading-7 tracking-wide" style={{color:'#6E8089'}}>
+
+          <p
+            className="py-8 leading-7 tracking-wide"
+            style={{ color: "#6E8089" }}
+          >
             {job?.description}
           </p>
           <h3
@@ -118,7 +126,8 @@ export const JobDesc = ({ isDarkMode }) => {
           <p className="pb-4 leading-7 tracking-wide text-gray-700">
             {job?.requirements.content}
           </p>
-          <ul className="font-bold" style={{color:'#6E8089'}}>
+
+          <ul className="font-bold" style={{ color: "#6E8089" }}>
             {job?.requirements.items.map((item, index) => (
               <li
                 className="pl-4 ml-4 leading-7 list-disc text-indigo-500"
@@ -127,15 +136,15 @@ export const JobDesc = ({ isDarkMode }) => {
                 <span
                   key={`${item}-${index}`}
                   className="font-normal "
-                  style={{color:'#6E8089'}}
+                  style={{ color: "#6E8089" }}
                 >
-                  
                   {" "}
                   {item}
                 </span>
               </li>
             ))}
           </ul>
+
           <h4
             className={`py-6 text-lg font-semibold tracking-wider${
               isDarkMode ? " text-white" : " text-black"
@@ -143,7 +152,10 @@ export const JobDesc = ({ isDarkMode }) => {
           >
             What You Will Do
           </h4>
-          <p className="pb-4 leading-7 tracking-wide" style={{color:'#6E8089'}}>
+          <p
+            className="pb-4 leading-7 tracking-wide"
+            style={{ color: "#6E8089" }}
+          >
             {job?.role.content}
           </p>
           <ol className="font-bold text-indigo-500">
@@ -155,7 +167,7 @@ export const JobDesc = ({ isDarkMode }) => {
                 <span
                   key={`${item}-${index}`}
                   className="font-normal"
-                  style={{color:'#6E8089'}}
+                  style={{ color: "#6E8089" }}
                 >
                   {item}
                 </span>
@@ -163,9 +175,22 @@ export const JobDesc = ({ isDarkMode }) => {
             ))}
           </ol>
         </div>
-        {/* apply now footer  */}
-        
+
+        {/* Footer content */}
+        <footer className="bg-white py-8 w-full ">
+          <div className="container mx-auto flex justify-center items-center">
+            <button className="bg-indigo-500 hover:bg-indigo-300 text-white w-[380px] h-12 rounded-md">
+              Apply Now
+            </button>
+          </div>
+        </footer>
       </section>
     </div>
   );
 };
+
+//   <footer >
+//   <div>
+//     <button></button>
+//   </div>
+// </footer>
